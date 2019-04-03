c145b3540c58af17a52ea9d79f53434dbfcb94f6
yuanhao.cui <yuanhao.cui@tcredit.com>
2019-04-03 23:32:00 +0800

    commit

---
+++ src/main/java/com/tcredit/Test.java
@@ -0,0 +1,13 @@
+package com.tcredit;
+
+import java.io.IOException;
+
+import org.dom4j.DocumentException;
+
+public class Test {
+	public static void main(String[] args) throws DocumentException, IOException {
+//		hashName : 'sha256',
+//        encoding : 'hex'
+	}
+
+}
