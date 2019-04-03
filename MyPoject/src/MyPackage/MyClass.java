72a555e24fb6d2207ee182d67e4438428ca259b3
sunxiangyou123 <yushuyuyou@aliyun.com>
2019-04-04 00:11:45 +0800

    a

---
+++ MyPoject/src/MyPackage/MyClass.java
@@ -0,0 +1,9 @@
+package MyPackage;
+
+public class MyClass {
+	public static void main(String[] arge) {
+		System.out.println("HelloWorld");		
+		
+	}
+
+}
---
+++ MyPoject/src/MyPackage/Test.java
@@ -0,0 +1,28 @@
+package MyPackage;
+
+public class Test {
+	public static void main(String[] arge) {
+		System.out.print("Hello");
+		System.out.println("World");
+		System.out.println("欢迎来到达内");
+		System.out.println("今日事今日毕");
+		System.out.print("...");
+		System.out.println("嘤嘤嘤");
+		System.err.println("NB");
+		System.out.println("结束");
+		System.out.println("并没有");
+		System.out.print("最强大脑");
+		System.out.println("a");
+		System.out.print("b");
+		System.out.println("c");
+		System.out.println("d");
+		System.out.println("f");	
+		System.err.print("e");
+		System.out.print("f");
+		System.out.println("HelloChina");
+		System.err.print("ChinaNO.1");
+		
+		
+	}
+
+}
