3765fc422d102b5eee5d24e469dee05f8faa0385
sunxiangyou123 <yushuyuyou@aliyun.com>
2019-04-03 23:17:36 +0800

    就业班1

---
+++ JSD1903/src/Day02/VarDemo.java
@@ -0,0 +1,39 @@
+package Day02;
+
+
+public class VarDemo {
+
+	public static void main(String[] args) {
+		 /*int a;
+		 int b,c;
+		 int d = 25;
+		 int e;
+		 e = 25;
+		 int f = 10;
+		 int g = f + 100;
+		 System.out.println(g);
+		 f = f + 100;
+		 System.out.println(f);
+		 //int h = 265.678;
+		 //System.out.println(i);
+		 int i;
+		 //System.out.print(i);
+		 //int b&o,123dd,^dk,h@,hjhss_3!,^_^;
+		 int age,money,i1,o_o;
+		 */
+		int a;
+		int b,c;
+		int d = 25;
+		int e;
+		e = 25;
+		int f =10;
+		int g = f+100;
+		System.out.print(g);
+		f = f +100;
+		System.out.println(f);
+		 
+		
+
+	}
+
+}
